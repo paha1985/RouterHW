f7c783fa0a75069ae12a93dc5387a300cb0e4fd5
paha1985 <paha1985@inbox.ru>
2025-06-17 14:05:56 +0800

    feat: create auth provider

---
+++ src/context/auth-provider.js
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const signin = (newUser, callback) => {
+    setUser(newUser);
+    callback();
+  };
+
+  const signout = (callback) => {
+    setUser(null);
+    callback();
+  };
+
+  const value = {
+    user,
+    signin,
+    signout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
